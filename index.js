29e19145cf0a09a6aa04ee1fb54a37dd63f0c746
mphxn <>
2022-08-13 23:33:34 +0000

    finished project

--- index.js
+++ index.js
@@ -1,19 +1,50 @@
-var http = require("http"); 
-http.createServer(function(req,res) {
-  var path = req.url.toLowerCase();
-  switch(path) {
-    case '/':
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end('Home page');
-      break;
-    case '/about':
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end('About page');
-      break;
-    default:
-      res.writeHead(404, {'Content-Type': 'text/plain'});
-      res.end('Not found');
-      break;
-    }
-}).listen(process.env.PORT || 3000);
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.set('view engine', 'ejs')
+
+//this will allow us to serve up static files, CSS, images & JS
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+  var title = "My Home Page";
+  var heading = "My Website";
+  res.render('pages/index', {
+    'title':title,
+    'heading':heading
+  })
+})
+
+app.get('/amethyst', (req, res) => {
+  var title = "Amethyst Page";
+  var heading = "My Website"
+    res.render('pages/amethyst', {
+    'title':title,
+    'heading':heading
+  })
+})
+
+app.get('/rose-quartz', (req, res) => {
+  var title = "Rose Quartz Page";
+  var heading = "My Website"
+    res.render('pages/rose-quartz', {
+    'title':title,
+    'heading':heading
+  })
+})
+
+app.get('/sunstone', (req, res) => {
+  var title = "Sunstone Page";
+  var heading = "My Website"
+    res.render('pages/sunstone', {
+    'title':title,
+    'heading':heading
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
 
